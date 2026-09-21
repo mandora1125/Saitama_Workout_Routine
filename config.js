a29e0c0d13f221d76c33fdd6bda2e86ceff9448e
mandora1125 <mandora1015@gmail.com>
2026-09-22 01:42:54 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Google Apps Script 웹앱의 /exec 주소를 넣으세요.
+// 예: https://script.google.com/macros/s/xxxxxxxxxxxxxxxx/exec
+window.SAITAMA_API_URL = 'https://script.google.com/macros/s/AKfycbwYebmaysLss5XMYd_LaiZZucwaLnHHVW8IqLAZ_HvfQCLTpkfSYxlH-tPZRdLQDX0x/exec';
